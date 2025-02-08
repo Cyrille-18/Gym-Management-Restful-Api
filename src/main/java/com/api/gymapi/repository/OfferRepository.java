316d2c311b0c434a75bb46180237286cef19eebd
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-08 19:32:18 +0000

    Style:Update methods

--- src/main/java/com/api/gymapi/repository/OfferRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.gymapi.repository;
-
-import com.api.gymapi.models.Pack;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfferRepository extends JpaRepository<Pack, Long> {
-}
--- src/main/java/com/api/gymapi/service/IUserService.java
+++ src/main/java/com/api/gymapi/service/IUserService.java
@@ -1,11 +1,17 @@
 package com.api.gymapi.service;
 
+import com.api.gymapi.Dtos.CustomerDto;
+import com.api.gymapi.Dtos.EmployeeDto;
+import com.api.gymapi.models.Employee;
 import com.api.gymapi.models.User;
 
 public interface IUserService {
-    public User getUser(long id);
-    public User getUserByUsername(String username);
-    public User createUser(User user);
-    public User updateUser(User user);
-    public User deleteUser(long id);
+    public User registerUserForCustomer(CustomerDto customerDto);
+    public boolean checkedPassword(String rawPassword, String encodedPassword);
+    public Employee registerUserForEmployee(EmployeeDto employeeDto);
+    public void updatePassword(User user, String newPassword);
+//    public User getUserByUsername(String username);
+//    public User createUser(User user);
+//    public User updateUser(User user);
+//    public User deleteUser(long id);
 }
