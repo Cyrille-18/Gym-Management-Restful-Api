3c5e451ffb8fa322a5267f4e2f06692202fd8b2f
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-13 00:03:12 +0000

    Feat: Enhance security configuration with JWT authentication and protect specific endpoints

--- src/main/java/com/api/gymapi/security/SecurityConfig.java
+++ src/main/java/com/api/gymapi/security/SecurityConfig.java
@@ -1,21 +1,35 @@
 package com.api.gymapi.security;
 
+import com.api.gymapi.service.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtService jwtService;
+
+    public SecurityConfig(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Désactive toute restriction
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/usermanagement/clients").authenticated() // 🔒 Protéger cet endpoint
+                        .requestMatchers("/auth/login").permitAll() // 🔓 Autoriser l’authentification
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtAuthFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
