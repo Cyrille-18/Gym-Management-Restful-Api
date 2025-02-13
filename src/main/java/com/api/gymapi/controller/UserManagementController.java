cda0e860be1a0bc7824da0962a91b99ff452a06b
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-13 00:01:33 +0000

    Feat: Update UserManagementController with customer management endpoints and authentication

--- src/main/java/com/api/gymapi/controller/UserManagementController.java
+++ src/main/java/com/api/gymapi/controller/UserManagementController.java
@@ -1,17 +1,22 @@
 package com.api.gymapi.controller;
 
+import com.api.gymapi.Dtos.CustomerDto;
 import com.api.gymapi.models.Customer;
 import com.api.gymapi.models.Employee;
 import com.api.gymapi.repository.CustomerRepository;
 import com.api.gymapi.repository.EmployeeRepository;
+import com.api.gymapi.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/usermanagement")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/users")
 public class UserManagementController {
 
     @Autowired
@@ -19,16 +24,45 @@ public class UserManagementController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private CustomerService customerService;
 
     /**
-     * Endpoint pour récupérer la liste des clients
+     * Endpoint pour récupérer la liste des clients (seulement pour les utilisateurs connectés)
      */
-    @GetMapping("/clients")
+    @GetMapping("/customers")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<Customer>> getAllCustomers() {
         List<Customer> customers = customerRepository.findAll();
         return ResponseEntity.ok(customers);
     }
+    /**
+     * Endpoint pour supprimer un client
+     */
 
+    @DeleteMapping("/customers/delete/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> deleteCustomer(@PathVariable long id) {
+        try {
+            customerRepository.deleteById(id);
+            return ResponseEntity.ok("Client supprimé avec succès.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+    /**
+     * Endpoint pour modifier un client
+     */
+    @PutMapping("/customers/update/{id}")
+    @PreAuthorize("isAuthenticated()") // Seuls les utilisateurs connectés peuvent modifier un client
+    public ResponseEntity<?> updateCustomer(@PathVariable Long id, @RequestBody CustomerDto customerDto) {
+        try {
+            Customer updatedCustomer = customerService.updateCustomer(id, customerDto);
+            return ResponseEntity.ok(updatedCustomer);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
     /**
      * Endpoint pour récupérer la liste des employés
      */
